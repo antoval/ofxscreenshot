8e408ff7d813e4981cd5ed8afbdc04a0bff3fdfa
info@apollomedia.nl <info@apollomedia.nl@0a4ecbfe-d7b1-11de-8e5c-7bc2cb6520f4>
2009-11-22 21:52:19 +0000

    Added files to svn

---
+++ trunk/src/ofxScreenshot.h
@@ -0,0 +1,36 @@
+/**
+ * Addon to take screenshots from your application. I created this addon,
+ * based on some information I found in the forum of openFrameworks a while
+ * ago. If you recognize your code, please tell me I'll add you as the author.
+ *
+ * I add this code into an addon as I wanted to be able to quickly create
+ * screenshots for my videos.
+ *
+ * @date 2009.11.20
+ * @author	Diederick Huijbers <diederick@apollomedia.nl>
+ *
+ */
+
+#ifndef OFXSCREENSHOTH
+#define OFXSCREENSHOTH
+#include "ofMain.h"
+
+class ofxScreenshot {
+public:
+	string 		filename;
+	int 		frame_count;
+	ofImage 	img;
+	float 		seconds;
+	float		minutes;
+
+	ofxScreenshot(string sFileName);
+	void 		grabFrame();
+	void 		pause();
+	void 		record();
+	void 		setPaused(bool bPaused);
+	bool 		isPaused();
+
+private:
+	bool paused;
+};
+#endif
